b79e497c8180d76424cb30d4249a8f6939efe06d
Yvan974 <54982605+Yvan974@users.noreply.github.com>
2019-09-27 09:27:25 +0400

    Add files via upload

--- dbconfig.php
+++ dbconfig.php
@@ -1,42 +1,16 @@
 <?php
-  // Déclaration d'une nouvelle classe
-  class connexionDB {
-    private $host    = 'localhost';   // nom de l'host
-    private $name    = 'connectanou';     // nom de la base de donnée
-    private $user    = 'root';        // utilisateur
-    private $pass    = '';        // mot de passe
-    //private $pass    = '';          // Ne rien mettre si on est sous windows
-    private $connexion;
-                    
-    function __construct($host = null, $name = null, $user = null, $pass = null){
-      if($host != null){
-        $this->host = $host;           
-        $this->name = $name;           
-        $this->user = $user;          
-        $this->pass = $pass;
-      }
-      try{
-        $this->connexion = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->name,
-          $this->user, $this->pass, array(PDO::MYSQL_ATTR_INIT_COMMAND =>'SET NAMES UTF8', 
-          PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
-      }catch (PDOException $e){
-        echo 'Erreur : Impossible de se connecter  à la BDD !';
-        die();
-      }
-    }
-    
-    public function query($sql, $data = array()){
-      $req = $this->connexion->prepare($sql);
-      $req->execute($data);
-      return $req;
-    }
-    
-    public function insert($sql, $data = array()){
-      $req = $this->connexion->prepare($sql);
-      $req->execute($data);
-    }
-  }
-  
-  // Faire une connexion à votre fonction
-  $DB = new connexionDB();
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'connectanou');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
 ?>
