fd83626202cca75068c7e002077c783316070050
Aym <aymeric.cy@gmail.com>
2019-10-10 15:54:50 +0400

    Modif img por et rea
    Modification image porteur et rea

---
+++ connectanou_laravel/realisateur.blade.php
@@ -0,0 +1,43 @@
+{{-- héritage de la view layouts/app.blade.php --}}
+@extends('layouts.app')
+
+@section('content')
+    
+
+<section>
+        <div class="container text-justify" ><br><br>
+          <h1 style="text-align: center;">Vous êtes étudiant ou jeune diplomé ?</h1><br><br>
+          <div class="container-fluid">
+            <img src="{{ asset('img/landing_rea.png')}}" class="col-md-10 col-lg-10 col-sm-10 col-xs-10" alt="realisateur de projets">
+          </div>
+          <div class="row">
+            <div class="col-md-12 col-lg-12 col-sm-12 col-xs-12">
+      
+              <FONT size="4pt"> 
+              
+                <p>Tu dois réaliser un<strong> projet</strong> à l’école mais tu n’as pas de sujet ? Tu cherches un emploi mais tu n'as pas assez d'<strong>expérience</strong> ? Les employeurs ne te font pas <strong>confiance</strong> ? Tu recherches de la mise en pratique sur une mission sympa ? Tu ne trouves pas de stages ? alternance ? Travail ? Alors comment embellir ton CV ?</p><br>
+      
+                <p><strong>Connectanou</strong> te propose des missions qui proviennent des <strong>projets de petites entreprises ou associations réunionnaises</strong> qui n'ont pas forcément les moyens et les compétences. L'idée est de les aider tout en montant en <strong>compétences.</strong><br>
+                Nos projets peuvent se réaliser à distance, en <strong>télétravail</strong> depuis chez toi ou à l'école, seul ou à plusieurs. Vous choisissez les projets qui vous intéresse et les petites entreprises que vous voulez aider.Voir les types de projets proposés (exemple :<strong> site web, logo,application mobile, référencement, étude de marché, sécrétariat, etc...</strong>).</p>
+                <br>
+                <P>Chaque contribution vous rapporte des points. Plus vous avez de points, plus vous monter de niveaux, plus vous êtes pris au sérieux. Vous pouvez échanger vos points contre des bons cadeaux ou réduction(s) chez<strong> CONNECTANOU </strong>ou chez nos <strong> partenaires.</strong></P><br>
+                 
+                <h2>CONNECTANOU Comment ça marche concrètement?</h2><br>
+                <p>Vous pouvez rechercher et parcourir tous les <strong>projets</strong> déposés par les <strong>entrepreneurs</strong>, choisir celui ou ceux qui répondent à vos compétences ou vos valeurs. Par contre, pour entrer en contact et négocier avec le porteur de projet, il faut être inscrit sur <strong>CONNECTANOU</strong>. Une fois le projet réalisé, vous recevrez le nb de points sur votre profil. Si l'évaluation est excellente, vous recevrez des points supplémentaires.</p><br>
+                <p>Vous pouvez également rédiger des articles (conseils sur une de vos compétences) pour <!-- <a href=""> -->le blog de Connectanou</a>, vous serez récompensez également en points.</p><br>
+      
+      
+                <p>Tu es <strong>formateur</strong> et tu recherches des <strong>projets enrichissants</strong> à donner en TP à tes élèves ? Marre des exercices fictifs à donner à tes élèves ? Tu souhaites motiver tes étudiants sur des<strong> projets sympas</strong> et qui ont du sens ? Choisir un ou plusieurs projets à donner à tes classes et <strong>accompagne</strong> les à <strong>réaliser</strong> leur <strong>mission</strong>.</p><br>
+                <p>N’hésitez pas à nous <a href="contact.php">contacter</a>.</p>
+              </FONT>
+              <p> &nbsp; </p>
+              <p>  &nbsp;</p>
+              <p>  &nbsp;</p>
+            </div>
+          </div> 
+        </div>
+      </section>
+      
+      
+      
+@endsection
